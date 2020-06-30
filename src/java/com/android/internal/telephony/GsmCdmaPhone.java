ad6be459b0ad94e6b7fe2df852ab4b8bc4e83fb5
Huilong Yin <huilong.yin@mediatek.com>
2020-06-30 15:12:10 +0800

    Change excepted result on R for testVoiceMailNumber
    If phone type is not gsm, getVoiceMailAlphaTag() will always
    return the string of R.string.defaultVoiceMailAlphaTag. So it
    does not read it from SIMrecords, it get default value “Voicemail”.
    
    Bug: 160386097
    Change-Id: I5a2e7ccb2e5652db06274d7eb1874bc72508538b
    (cherry-picked from 5ec720f95f0b80e65db43f3a08794e77fff4c207)

--- src/java/com/android/internal/telephony/GsmCdmaPhone.java
+++ src/java/com/android/internal/telephony/GsmCdmaPhone.java
@@ -1736,8 +1736,8 @@ public class GsmCdmaPhone extends Phone {
     public String getVoiceMailAlphaTag() {
         String ret = "";
 
-        if (isPhoneTypeGsm()) {
-            IccRecords r = mIccRecords.get();
+        if (isPhoneTypeGsm() || mSimRecords != null) {
+            IccRecords r = isPhoneTypeGsm() ? mIccRecords.get() : mSimRecords;
 
             ret = (r != null) ? r.getVoiceMailAlphaTag() : "";
         }
